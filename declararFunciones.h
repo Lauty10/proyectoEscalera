f1ec29b455b12ffb6336591a7e9ba5a880d8a829
Lautaro Maldonado <lautylbs@hotmail.com>
2024-06-14 17:47:58 -0300

    optimizacion

--- declararFunciones.h
+++ declararFunciones.h
@@ -15,4 +15,5 @@ void juegoUno(std::string jugador1,std::string nombre,int sumaTotal, int vectorD
 void juegoDos(std::string nombre,std::string nombreDos,std::string jugador1,std::string jugador2,int sumaTotal1,int vectorD[6],int opcion,int puntoLanzamiento1,int multiplicarPuntos,int band,int maxNum,int sumaTotal2,int puntosLanzamiento2,int band1,int maxNum2,int bandPuntajeDos,int&maxPuntajeDos);
 void mostrarPuntajes(int&maxPuntaje,int&maxPuntajeDos);
 void modoSimulado(std::string jugador1,std::string nombre,int vectorD[6],int opcion,int puntosLanzamiento1,int sumaTotal,int multiplicarPuntos,int band,int maxNum);
+void resultado(int escalera,int igualesSeis,int distintosSeis,int sumaDados,int opcion,int&puntosLanzamiento1,int&sumaTotal,int multiplicarPuntos);
 #endif // DECLARARFUNCIONES_H_INCLUDED
